fb3e2a94dbed75068a74dd53b34717a1908b319a
Adithya V <adi_101@live.com>
2017-02-04 18:47:05 -0500

    added route selection to google maps

--- potholemap.js
+++ potholemap.js
@@ -1,86 +1,102 @@
+// This example requires the Visualization library. Include the
+// libraries=visualization parameter when you first load the API. For example:
+// <script
+// src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=visual
+// ization">
 
-var map;
+var map, heatmap;
 
 function initMap() {
     map = new google
         .maps
         .Map(document.getElementById('map'), {
+            zoom: 15,
             center: {
                 lat: 44.2253,
                 lng: -76.4951
             },
-            zoom: 15,
-            styles: [
-                {
-                    featureType: 'poi',
-                    stylers: [
-                        {
-                            visibility: 'off'
-                        }
-                    ] // Turn off points of interest.
-                }, {
-                    featureType: 'transit.station',
-                    stylers: [
-                        {
-                            visibility: 'off'
-                        }
-                    ] // Turn off bus stations, train stations, etc.
-                }
-            ],
-            disableDoubleClickZoom: true
+            mapTypeId: 'roadmap'
         });
 
-     function makeInfoBox(controlDiv, map) {
-    // Set CSS for the control border.
-    var controlUI = document.createElement('div');
-    controlUI.style.boxShadow = 'rgba(0, 0, 0, 0.298039) 0px 1px 4px -1px';
-    controlUI.style.backgroundColor = '#fff';
-    controlUI.style.border = '2px solid #fff';
-    controlUI.style.borderRadius = '2px';
-    controlUI.style.marginBottom = '22px';
-    controlUI.style.marginTop = '10px';
-    controlUI.style.textAlign = 'center';
-    controlDiv.appendChild(controlUI);
+    heatmap = new google.maps.visualization.HeatmapLayer({
+        data: getPoints(), 
+        map: map,
+        radius: 18
+    });
+    // ------------------
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    var directionsService = new google.maps.DirectionsService;
+    directionsDisplay.setMap(map);
+    directionsDisplay.setPanel(document.getElementById('right-panel'));
+    var control = document.getElementById('floating-panel');
 
-    // Set CSS for the control interior.
-    var controlText = document.createElement('div');
-    controlText.style.color = 'rgb(25,25,25)';
-    controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
-    controlText.style.fontSize = '100%';
-    controlText.style.padding = '6px';
-    controlText.innerText = 'The map shows all clicks made in the last 10 minutes.';
-    controlUI.appendChild(controlText);
-  }
-  
-    var infoWindow = new google
-        .maps
-        .InfoWindow({map: map});
+    control.style.display = 'block';
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
 
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-        navigator
-            .geolocation
-            .getCurrentPosition(function (position) {
-                var pos = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude
-                };
-
-                infoWindow.setPosition(pos);
-                infoWindow.setContent('Location found.');
-                map.setCenter(pos);
-            }, function () {
-                handleLocationError(true, infoWindow, map.getCenter());
-            });
-    } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-    }
+    var onChangeHandler = function() {
+        calculateAndDisplayRoute(directionsService, directionsDisplay);
+    };
+    document.getElementById('start').addEventListener('change', onChangeHandler);
+    document.getElementById('end').addEventListener('change', onChangeHandler);
+    //----------------------
 }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation
-        ? 'Error: The Geolocation service failed.'
-        : 'Error: Your browser doesn\'t support geolocation.');
+function toggleHeatmap() {
+    heatmap.setMap(heatmap.getMap()
+        ? null
+        : map);
+}
+
+// changes the colours of the heatmap
+function changeGradient() {
+    var gradient = [
+        'rgba(0, 255, 255, 0)',
+        'rgba(0, 255, 255, 1)',
+        'rgba(0, 191, 255, 1)',
+        'rgba(0, 127, 255, 1)',
+        'rgba(0, 63, 255, 1)',
+        'rgba(0, 0, 255, 1)',
+        'rgba(0, 0, 223, 1)',
+        'rgba(0, 0, 191, 1)',
+        'rgba(0, 0, 159, 1)',
+        'rgba(0, 0, 127, 1)',
+        'rgba(63, 0, 91, 1)',
+        'rgba(127, 0, 63, 1)',
+        'rgba(191, 0, 31, 1)',
+        'rgba(255, 0, 0, 1)'
+    ]
+    heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+}
+
+function changeRadius() {
+    heatmap.set('radius', heatmap.get('radius') ? null : 20);
+}
+
+function changeOpacity() {
+    heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
+}
+
+// Heatmap data: 500 Points
+function getPoints() {
+    return [
+        new google.maps.LatLng(44.2253, -76.4951),
+        new google.maps.LatLng(44.2260, -76.4954)
+    ];
+}
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+        var start = document.getElementById('start').value;
+        var end = document.getElementById('end').value;
+        directionsService.route({
+          origin: start,
+          destination: end,
+          travelMode: 'DRIVING'
+        }, 
+        function(response, status) {
+          if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
 }
