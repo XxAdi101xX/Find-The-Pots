f10d02bba2c6a8b6aec1affda02978fac8293ad3
Adithya V <adi_101@live.com>
2017-02-05 01:55:09 -0500

    subs testing

--- gs_pots_server/1-hello-world/app.js
+++ gs_pots_server/1-hello-world/app.js
@@ -58,13 +58,10 @@ function listTopics () {
     }).catch(console.log);
 }
 
-setIntervallistTopics();
+listTopics();
 
 let messages = [];
 let count = 0;
-// pubsub.on('message', function(messageIn) {
-//   message = messageIn;
-// })
 
 setInterval(function() {
   messages.push(count++)
@@ -86,9 +83,36 @@ function createSubscription (topicName, subscriptionName) {
   return topic.subscribe(subscriptionName)
     .then((results) => {
       const subscription = results[0];
-
       console.log(`Subscription ${subscription.name} created.`);
-
       return subscription;
     });
 }
+
+createSubscription('accel_data', 'main-sub');
+
+function pullMessages (subscriptionName) {
+  // Instantiates a client
+  const pubsub = PubSub();
+
+  // References an existing subscription, e.g. "my-subscription"
+  const subscription = pubsub.subscription(subscriptionName);
+
+  // Pulls messages. Set returnImmediately to false to block until messages are
+  // received.
+  return subscription.pull()
+    .then((results) => {
+      const messages = results[0];
+
+      console.log(`Received ${messages.length} messages.`);
+
+      messages.forEach((message) => {
+        console.log(`* %d %j %j`, message.id, message.data, message.attributes);
+      });
+
+      // Acknowledges received messages. If you do not acknowledge, Pub/Sub will
+      // redeliver the message.
+      return subscription.ack(messages.map((message) => message.ackId));
+    });
+}
+
+pullMessages('main-sub');
