6d9a7a847b2e294d310a0a07a261050ffbaa21e3
Adithya V <adi_101@live.com>
2017-02-05 09:26:02 -0500

    added gps tracking for current location

--- gs_pots_server/1-hello-world/potholemap.js
+++ gs_pots_server/1-hello-world/potholemap.js
@@ -38,6 +38,26 @@ function initMap() {
     document.getElementById('start').addEventListener('change', onChangeHandler);
     document.getElementById('end').addEventListener('change', onChangeHandler);
     //----------------------
+     var infoWindow = new google.maps.InfoWindow({map: map});
+
+        // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Location found.');
+        map.setCenter(pos);
+        }, function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
 }
 
 function toggleHeatmap() {
@@ -197,5 +217,10 @@ function getUpdate() {
 
 //setInterval(getUpdate, 1000);
 
-
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                    'Error: The Geolocation service failed.' :
+                    'Error: Your browser doesn\'t support geolocation.');
+}
 
